07ed8cb6324ae9d3ac6f1d3b6ebd20efb1859e16
Nithyasri-2516 <nithyasri1625@gmail.com>
2024-10-07 17:48:16 +0530

    add files

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -9,6 +9,28 @@ export class AdminLoginComponent {
     firstname="nithu"
     lastname="as"
     title="learn angular"
+
     addr="13,bbcstreet"
+    color="blue"  
+
+    isDisabled= false
+
+
+    getName(){
+      return this.firstname;
+    }
+    
+    getMax(a:number, b:number){
+      return Math.max(a,b);
+    }
+
+    clickCount=0
+    click(){
+      this.clickCount++;
+    }
+
+    
+
+    
 
 }
