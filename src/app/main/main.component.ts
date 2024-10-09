6ddeb53510b70d914694c435da62b25e516843ce
Nithyasri-2516 <nithyasri1625@gmail.com>
2024-10-09 16:57:18 +0530

    add  new files

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+
+  //ng-for
     title="list"
     movies=["unakum enakum","sachin","kutty","Ghilli","goat"]
 
@@ -16,4 +18,17 @@ export class MainComponent {
       {name:"jemi", age:"90"},
       {name:"kavi", age:"100"}
     ]
+//ng-switch
+num=0;
+
+showme= true;
+show=false;
+
+
+event=true
+enableBig=true
+
+enableColor= "red"
+
+enableBGColor= "blue"
 }
